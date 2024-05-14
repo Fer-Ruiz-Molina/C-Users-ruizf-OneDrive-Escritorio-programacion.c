844eded1a6fa61b7e527f7e653e06c346096df54
Fer-Ruiz-Molina <168505783+Fer-Ruiz-Molina@users.noreply.github.com>
2024-05-13 23:53:17 -0600

    Add files via upload

---
+++ trabajomarzo1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for (size_t i = 0; i < 101; i++)
+    {
+        printf("%d\n",i);
+    }
+}
---
+++ trabajomarzo6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int 1;
+    i=0;
+    while (i<100)
+    {
+        i=i+2;
+        printf("%d\n",i);
+    }
+}
